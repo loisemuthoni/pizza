ee28443d9429c4291cdb1c0c534066e062785ded
loisemuthoni <loisemburu01@gmail.com>
2019-11-11 16:08:32 +0300

    added total

--- Js/app.js
+++ Js/app.js
@@ -1,9 +1,19 @@
-var submitbutton = document.getElementById("submit2");
-submitbutton.click(function() {
-        var size = document.getElementById("sizes");
-        console.log(size)
-    })
-    // $("#submit2").click(function() {
-    //     var size = $("#sizes option:select").text();
-    //     console.log(size);
-    // })
+const add = document.getElementById("add");
+
+add.addEventListener("click", function() {
+    const size = document.getElementById("sizes");
+    const crust = document.getElementById("crusts");
+    const topping = document.getElementById("toppings");
+    const quantity = document.getElementById("quantity");
+})
+
+
+// var submitbutton = document.getElementById("submit2");
+// submitbutton.click(function() {
+//     var size = document.getElementById("sizes");
+//     console.log(size)
+// })
+// var addbutton = document.getElementById("add");
+// addbutton.click(function() {
+//             var add = document.getElementById("sub");
+//             console.log(sub)
