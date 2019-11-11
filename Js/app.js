f38369946186315c776f67a56c555980a335cb5a
loisemuthoni <loisemburu01@gmail.com>
2019-11-11 15:35:19 +0300

    added js

---
+++ Js/app.js
@@ -0,0 +1,9 @@
+var submitbutton = document.getElementById("submit2");
+submitbutton.click(function() {
+        var size = document.getElementById("sizes");
+        console.log(size)
+    })
+    // $("#submit2").click(function() {
+    //     var size = $("#sizes option:select").text();
+    //     console.log(size);
+    // })
