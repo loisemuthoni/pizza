190a1b70a3b7d573edfe65805c0b1c01545c5436
loisemuthoni <loisemburu01@gmail.com>
2019-11-12 10:29:03 +0300

    all changes

--- Js/app.js
+++ Js/app.js
@@ -1,28 +1,47 @@
 const add = document.getElementById("add");
 
 add.addEventListener("click", function() {
-    const size = document.getElementById("sizes");
-    const crust = document.getElementById("crusts");
-    const topping = document.getElementById("toppings");
-    const quantity = document.getElementById("quantity");
+    const size = parseInt(document.getElementById("sizes").value);
+    const crust = parseInt(document.getElementById("crusts").value);
+    const topping = parseInt(document.getElementById("toppings").value);
+    const quantity = parseInt(document.getElementById("quantity").value);
+
+    const results = function() {
+        const table = document.getElementById("table");
+        const row = table.insertRow();
+        const quantity1 = row.insertCell();
+        const size2 = row.insertCell();
+        const crust4 = row.insertCell();
+        const toppings3 = row.insertCell();
+        const total5 = row.insertCell();
+        quantity1.innerHTML = quantity;
+        size2.innerHTML = size;
+        crust4.innerHTML = crust;
+        toppings3.innerHTML = topping;
+        total5.innerHTML = (size + crust + topping) * quantity;
+        size2.innerHTML = size;
+        crust4.innerHTML = crust.options[crust.selectedIndex].text;
+        toppings3.innerHTML = topping;
+        quantity1.innerHTML = quantity.options[quantity.selectedIndex].text;
+
+    }
+    results();
 })
 
-Const results = function() {
-    const table = document.getElementById("table");
-    const row = table.insertRow();
-    const size1 = row.insertCell();
-    const crust2 = row.insertCell();
-    const toppings3 = row.insertCell();
-    const quantity4 = row.insertCell();
-    const total5 = row.insertCell();
-}
 
-// var submitbutton = document.getElementById("submit2");
-// submitbutton.click(function() {
-//     var size = document.getElementById("sizes");
-//     console.log(size)
-// })
-// var addbutton = document.getElementById("add");
-// addbutton.click(function() {
-//             var add = document.getElementById("sub");
-//             console.log(sub)
+
+
+const checkout = document.getElementById("checkout")
+
+checkout.addEventListener("click", function(event) {
+    event.preventDefault()
+    const delivery = document.getElementById("check");
+    const para = document.getElementById("para");
+    const deliveryInput = document.getElementById("mess").value;
+
+    if (delivery.checked) {
+        para.innerHTML = `Thank you for your purchase. Your order will be delivered at ${deliveryInput} for an extra 200 bob charge`
+    }
+
+
+})
